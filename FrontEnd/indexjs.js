366adbff40aeb7bf3f3797097351c77a63eb058e
Theepob <theepob15112@gmail.com>
2023-04-27 12:10:08 +0700

    now we can write new kratoo but sth weird

--- FrontEnd/indexjs.js
+++ FrontEnd/indexjs.js
@@ -9,11 +9,26 @@ let topics=[
     '<h2>topic7 : problem7</h2><div>how to ... problem7?</div>',
     '<h2>topic8 : problem8</h2><div>how to ... problem8?</div>'
 ];
+let addkratoo='<span>TOPIC :</span><input type="text" id="topic"><br><span>QUESTION :</span><textarea id="content" rows="4" cols="50"></textarea><br><button type="submit" onclick="addKratoo()">submit</button></div> '
 function toTopic(n){
-    var sth=document.getElementById("kratoo");
-    sth.innerHTML=topics[n];
+    var content=document.getElementById("kratoo");
+    content.innerHTML=topics[n];
 }
 function toMainmenu(){
-    var sth=document.getElementById("kratoo");
-    sth.innerHTML=main;
+    var content=document.getElementById("kratoo");
+    content.innerHTML=main;
 }
+function toAddKratooPage(){
+    var content=document.getElementById("kratoo");
+    content.innerHTML=addkratoo;
+}
+function addKratoo(){
+    let topic = document.getElementById("topic").value;
+    let content = document.getElementById("content").value; 
+    topics.push('<h2>'+topic+' : problem'+(topics.length+1)+'</h2><div>'+content+'</div>');
+    console.log(main);
+    main=main.slice(0,main.length-12)+'<a href="#" onclick="toTopic(8)"><li class="topic"><h2>topic9 : problem9</h2><div>how to ... problem9?</div</li></a>'+'</ul>';
+    console.log(main);
+    content=document.getElementById("kratoo");
+    content.innerHTML=main;
+ }
