473ab4c0d5283bc70b58c127622edbb5982afb1e
POOMMMMMM <poom99.2002@gmail.com>
2023-04-29 06:31:07 +0700

    login page

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 const backendIPAddress = "52.20.54.127:3000";
 
 const authorizeApplication = () => {
-  //   window.location.href = `http://${backendIPAddress}/courseville/auth_app`;
-  console.log("authorizeApplication");
+  window.location.href = `http://${backendIPAddress}/courseville/auth_app`;
+  //   console.log("authorizeApplication");
 };
