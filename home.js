bc5cb734e7252177b342f1744c634b3e87d319f4
POOMMMMMM <poom99.2002@gmail.com>
2023-04-29 22:24:53 +0700

    edit home

--- home.js
+++ home.js
@@ -12,11 +12,11 @@ const getUserProfile = async () => {
     .then((data) => {
       console.log(data.user);
       document.getElementById(
-        "text_info"
-      ).innerHTML = `Thammasorn Thammasarangkoon`; //`${data.user.title_en} ${data.user.firstname_en} ${data.user.lastname_en}`;
+        "eng-name-info"
+      ).innerHTML = `${data.user.title_en} ${data.user.firstname_en} ${data.user.lastname_en}`;
       document.getElementById(
         "thai-name-info"
-      ).innerHTML = `ธรรมสรณ์ ธรรมสรางกูร`; //`${data.user.title_th} ${data.user.firstname_th} ${data.user.lastname_th}`;
+      ).innerHTML = `${data.user.title_th} ${data.user.firstname_th} ${data.user.lastname_th}`;
     })
     .catch((error) => console.error(error));
 };
