3d890a9ff73f7ef5340221d35af05f49d71697bd
POOMMMMMM <poom99.2002@gmail.com>
2023-04-29 22:07:44 +0700

    edit home

--- home.js
+++ home.js
@@ -1,22 +1,22 @@
 const backendIPAddress = "52.20.54.127:3000";
 const getUserProfile = async () => {
-    const options = {
-        method: "GET",
-        credentials: "include",
-    };
-    await fetch(
-        `http://${backendIPAddress}/courseville/get_profile_info`,
-        options
-    )
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data.user);
-            document.getElementById(
-                "eng-name-info"
-            ).innerHTML = `${data.user.title_en} ${data.user.firstname_en} ${data.user.lastname_en}`;
-            document.getElementById(
-                "thai-name-info"
-            ).innerHTML = `${data.user.title_th} ${data.user.firstname_th} ${data.user.lastname_th}`;
-        })
-        .catch((error) => console.error(error));
+  const options = {
+    method: "GET",
+    credentials: "include",
+  };
+  await fetch(
+    `http://${backendIPAddress}/courseville/get_profile_info`,
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.user);
+      document.getElementById(
+        "text_info"
+      ).innerHTML = `Thammasorn Thammasarangkoon`; //`${data.user.title_en} ${data.user.firstname_en} ${data.user.lastname_en}`;
+      document.getElementById(
+        "thai-name-info"
+      ).innerHTML = `ธรรมสรณ์ ธรรมสรางกูร`; //`${data.user.title_th} ${data.user.firstname_th} ${data.user.lastname_th}`;
+    })
+    .catch((error) => console.error(error));
 };
