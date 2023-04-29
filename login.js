aa932725064a8b618d992830e381c0ad246a04e7
unknown <peeraphat4106@gmail.com>
2023-04-29 17:35:19 +0700

    homepage

--- login.js
+++ login.js
@@ -2,5 +2,4 @@ const backendIPAddress = "52.20.54.127:3000";
 
 const authorizeApplication = () => {
   window.location.href = `http://${backendIPAddress}/courseville/auth_app`;
-  //   console.log("authorizeApplication");
 };
