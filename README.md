172008f3ff6e731c6e4b20ac4429ee3bc4bdead2
POOMMMMMM <poom99.2002@gmail.com>
2023-04-29 22:37:18 +0700

    add file

---
+++ README.md
@@ -0,0 +1,2 @@
+# Final-Project-CEE-G04-FrontEnd
+Group 4
---
+++ login.js
@@ -0,0 +1,5 @@
+const backendIPAddress = "52.20.54.127:3000";
+
+const authorizeApplication = () => {
+  window.location.href = `http://${backendIPAddress}/courseville/auth_app`;
+};
